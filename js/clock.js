f8bdc93715767efbe83357a520286d58ae631c6c
mustbebuilt <martin@mustbebuilt.co.uk>
2023-08-16 15:14:04 +0100

    Improved code

--- js/clock.js
+++ js/clock.js
@@ -1,35 +1,37 @@
-(function(){
-// IIFE start
-console.info("Hello JS")
-var myNode = document.createElement("div");
-document.getElementById("digitalClock").appendChild(myNode);
+(() => {
+  // IIFE start
+  console.info("Hello JS");
+  const myNode = document.createElement("div");
+  const digitalClock = document.getElementById("digitalClock");
+  const secondHand = document.getElementById("secondHand");
+  const minHand = document.getElementById("minHand");
+  const hrHand = document.getElementById("hrHand");
 
-var updateTime = function(){
-    console.info('tick tock');
-    var myDate = new Date();
-    myNode.innerHTML = myDate.toTimeString().substring(0,8);
-    // seconds
-    var seconds = myDate.getSeconds();
-    var rotSeconds = (seconds * 6) - 90;
-    document.getElementById("secondHand").style.transform = `rotate(${rotSeconds}deg)`;
-    // minutes
-    var minutes = myDate.getMinutes();
-    var rotMins = (minutes * 6) -90;
-    document.getElementById("minHand").style.transform = `rotate(${rotMins}deg)`;
-    // hours
-    var hours = myDate.getHours();
-    //hours = hours + minutes/60;
-    hours += minutes/60;
-    var rotHours = (hours * 30) -90;
-    document.getElementById("hrHand").style.transform = `rotate(${rotHours}deg)`;
+  digitalClock.appendChild(myNode);
 
-}
-setInterval(updateTime, 1000);
-updateTime();
-/// analogue clock
-document.getElementById("hrHand").style.backgroundColor = "#0f0";
+  const updateClockHands = (myDate) => {
+    const seconds = myDate.getSeconds();
+    const minutes = myDate.getMinutes();
+    const hours = myDate.getHours() + minutes / 60;
 
+    const rotSeconds = seconds * 6 - 90;
+    const rotMins = minutes * 6 - 90;
+    const rotHours = hours * 30 - 90;
 
+    secondHand.style.transform = `rotate(${rotSeconds}deg)`;
+    minHand.style.transform = `rotate(${rotMins}deg)`;
+    hrHand.style.transform = `rotate(${rotHours}deg)`;
+  };
 
-// END
+  const updateTime = () => {
+    console.info("tick tock");
+    const myDate = new Date();
+    myNode.innerHTML = myDate.toLocaleTimeString([], { timeStyle: "medium" });
+    updateClockHands(myDate);
+  };
+
+  setInterval(updateTime, 1000);
+  updateTime();
+
+  // END
 })();
